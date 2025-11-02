b559f9019570ad773158adf9f24274a0e7ed28e3
STEFAN PETRU-STOICIU <76152219+PeTruStEfAN21@users.noreply.github.com>
2025-11-02 20:16:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,10 +37,10 @@ protected:
     }
 
 public:
-    jucator(const string &nume, const string &pozitie, int ovr, int spike_power, int receive, int spike_accuracy,
+    jucator(const string &num, const string &pozitie, int ovr, int spike_power, int receive, int spike_accuracy,
             int serve_power, int serve_accuracy, int vertical_jump, int mobility, int speed, int pret, int height,
             bool ales)
-        : nume(nume), pozitie(pozitie), ovr(ovr), spike_power(spike_power), receive(receive),
+        : nume(num), pozitie(pozitie), ovr(ovr), spike_power(spike_power), receive(receive),
           spike_accuracy(spike_accuracy), serve_power(serve_power), serve_accuracy(serve_accuracy),
           vertical_jump(vertical_jump), mobility(mobility), speed(speed), pret(pret), height(height),
           ales(ales) {}
@@ -127,12 +127,12 @@ jucator()
         set_pret();
     }
 
-    void set_nume(const string &nume) {
+    void set_num(const string &num) {
       this->nume = nume;
     }
 };
 
-// CORECȚIE: Alias-uri definite după jucator
+
 using JucatorPtr = shared_ptr<jucator>;
 using EchipaPtr = shared_ptr<Echipe>;
 
@@ -855,7 +855,7 @@ public:
     manageri(const manageri& other) = delete; 
     manageri& operator=(const manageri& other) = delete; 
 
-~manageri() = default; // shared_ptr se curăță automat
+~manageri() = default; 
 };
 
 
